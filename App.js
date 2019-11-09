8ddbe925175d6f864693cae39a58ab019ca10aa6
gatskaki <flaviomanuel258@gmail.com>
2019-11-09 15:24:39 -0200

    TrueLayout

--- App.js
+++ App.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet, TextInput } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, Image, Button } from 'react-native'
 
 export default class App extends Component{
+
   render(){
+    
     return (
       <View style={style.container}>
+
         <Text style={style.f20}>Demanda IZI</Text>
+        <Image source={require('./imgs/logoDemanda')}  style={{width: 140, height: 135}}/>
         
-      </View>
+        <View style={style.write}>
+        <TextInput  placeholder=" E-mail" style={style.input}/>
+        </View>
+        <View>
+        <TextInput placeholder=" Senha" style={style.input}/>
+        </View>
+
+        <View style={style.botao}>
+          <Button style={style.dentrobotao} title="Logar" onPress ={() => alert("Apertado!")}/>
+        </View>
+        
+        </View>
+      
     )
   }
 }
-// style
+
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 10,
     alignItems: 'center',
+    
   },
   f20: {
     fontSize: 20
+  },
+  input: {
+    height: 35,
+    width: 300,
+    borderColor: "gray",
+    borderWidth: 1,
+    backgroundColor: "#EDE7FD",
+    
+  },
+  write: {
+    marginTop: 100,
+    padding: 20
+  },
+  botao: {
+    margin: 20,
+    
+    
+  },
+
+  dentrobotao:{
+   
+
+
   }
 
 })
