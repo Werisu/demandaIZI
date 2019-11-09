2661dd53fbb0393c21f889a762bf0bc23175a61b
Werisu <wellysson35@gmail.com>
2019-11-09 10:03:19 -0300

    primeiro commit, projeto inicializado

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+
+export default class App extends Component{
+  render(){
+    return (
+      <View style={style.container}>
+        <Text style={style.f20}>Demanda IZI</Text>
+        
+      </View>
+    )
+  }
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    alignItems: 'center',
+  },
+  f20: {
+    fontSize: 20
+  }
+
+})
