50952572bd485f6e25793b22cad43f3c13ae35e5
Werisu <wellysson35@gmail>
2019-11-10 01:50:36 -0300

    Codificando BackEnd Java e Configurando o AndroidManifest.xml

--- app/src/main/java/com/example/demandaizi/MainActivity.java
+++ app/src/main/java/com/example/demandaizi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.demandaizi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        final Button cadastrar = findViewById(R.id.cadastrar);
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TelaCadastro1.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/demandaizi/TelaCadastro1.java
+++ app/src/main/java/com/example/demandaizi/TelaCadastro1.java
@@ -1,11 +1,13 @@
 package com.example.demandaizi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class TelaCadastro1 extends AppCompatActivity {
 
@@ -16,6 +18,14 @@ public class TelaCadastro1 extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final Button empregador = findViewById(R.id.empregador);
+        empregador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TelaCadastro1.this, TelaCadEmpregador.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
