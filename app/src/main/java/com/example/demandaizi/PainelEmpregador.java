5581ac5bccd0bee320d291e24a421a58607f8a8c
Werisu <wellysson35@gmail>
2019-11-10 05:34:02 -0300

    Criando a tela de proposta de serviços, fazendo melhorias, colocando mais funcionalidades nos dashboards

--- app/src/main/java/com/example/demandaizi/PainelEmpregador.java
+++ app/src/main/java/com/example/demandaizi/PainelEmpregador.java
@@ -1,11 +1,13 @@
 package com.example.demandaizi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class PainelEmpregador extends AppCompatActivity {
 
@@ -16,7 +18,14 @@ public class PainelEmpregador extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        Button proposta = findViewById(R.id.skills);
+        proposta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PainelEmpregador.this, PropostaDeServicos.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/demandaizi/PropostaDeServicos.java
@@ -0,0 +1,22 @@
+package com.example.demandaizi;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PropostaDeServicos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proposta_de_servicos);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
